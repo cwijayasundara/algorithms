7a227ee33aff72823bd18c329c7ffa7410efd400
cwijayasundara <cwijayasundara@gmail.com>
2017-02-01 11:31:43 +0000

    added factorial and qick sort

---
+++ python-algo/factorial.py
@@ -0,0 +1,8 @@
+# not tail recursive
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return x * factorial(x-1)
+
+print factorial(10)
---
+++ python-algo/quicksort.py
@@ -0,0 +1,14 @@
+def qsort(array):
+    if len(array) < 2:
+        # if array has 1 element its already sorted
+        return array
+    else:
+        # set the 1st elem as the pivot
+        pivot = array[0]
+        # sub array for elements less than the pivot
+        less = [i for i in array[1:] if i <= pivot]
+        # sub array for elements grater than the pivot
+        greater = [i for i in array[1:] if i > pivot]
+        return qsort(less) + [pivot] + qsort(greater)
+
+print qsort([23, 13, 345, 2000, 1, 0, 99])
